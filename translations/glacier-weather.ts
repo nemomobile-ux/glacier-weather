f5c94e407a82568acf369b7db30df39497f3c25e
Jozef Mlich <jmlich83@gmail.com>
2021-12-30 10:54:29 +0100

    translate desktop file

--- translations/glacier-weather.ts
+++ translations/glacier-weather.ts
@@ -5,10 +5,12 @@
     <name>MainPage</name>
     <message>
         <location filename="../src/qml/pages/MainPage.qml" line="13"/>
+        <location filename="../src/qml/pages/MainPage.qml" line="13"/>
         <source>Weather</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/MainPage.qml" line="93"/>
         <location filename="../src/qml/pages/MainPage.qml" line="93"/>
         <source>No weather data</source>
         <translation type="unfinished"></translation>
@@ -18,30 +20,36 @@
     <name>PlacesPage</name>
     <message>
         <location filename="../src/qml/pages/PlacesPage.qml" line="19"/>
+        <location filename="../src/qml/pages/PlacesPage.qml" line="19"/>
         <source>Places</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/PlacesPage.qml" line="43"/>
         <location filename="../src/qml/pages/PlacesPage.qml" line="43"/>
         <source>Add place</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/PlacesPage.qml" line="45"/>
+        <location filename="../src/qml/pages/PlacesPage.qml" line="45"/>
         <source>Ok</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/PlacesPage.qml" line="46"/>
         <location filename="../src/qml/pages/PlacesPage.qml" line="46"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/PlacesPage.qml" line="51"/>
+        <location filename="../src/qml/pages/PlacesPage.qml" line="51"/>
         <source>Enter name of city</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/PlacesPage.qml" line="68"/>
         <location filename="../src/qml/pages/PlacesPage.qml" line="68"/>
         <source>Trying to get position ...</source>
         <translation type="unfinished"></translation>
@@ -59,6 +67,7 @@
     <name>SettingsPage</name>
     <message>
         <location filename="../src/qml/pages/SettingsPage.qml" line="15"/>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="15"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
@@ -67,75 +76,90 @@
     <name>WeatherSettings</name>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="76"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="76"/>
         <source>Miles</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="76"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="76"/>
         <source>Mph</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="77"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="77"/>
         <source>Meters per seconds</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="77"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="77"/>
         <source>m/s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="78"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="78"/>
         <source>Kilometers per hour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="78"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="78"/>
         <source>km/h</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="79"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="79"/>
         <source>Knots</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="79"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="79"/>
         <source>kt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="84"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="84"/>
         <source>Kelvin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="84"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="84"/>
         <source>K</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="85"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="85"/>
         <source>Celsius</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="85"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="85"/>
         <source>°C</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="86"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="86"/>
         <source>Fahrenheit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/WeatherSettings.qml" line="86"/>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="86"/>
         <source>°F</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/WeatherSettings.qml" line="134"/>
         <location filename="../src/qml/components/WeatherSettings.qml" line="134"/>
         <source>Current position</source>
         <translation type="unfinished"></translation>
